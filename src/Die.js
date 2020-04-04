625f86a672d3e4b6df895822b91681cf85a47fb3
unknown <codewithsrobins1@gmail.com>
2020-04-03 20:38:01 -0700

    Initial File Upload

---
+++ src/Die.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 1
+  };
+
+  constructor(props){
+  super(props);
+
+  this.handleClick = this.handleClick.bind(this);
+  
+}
+
+
+handleClick(){
+  this.props.handleClick(this.props.idx)
+}
+
+  //Render
+  render() {
+    const {numberWords, locked, val, disabled, rolling} = this.props;
+     //grab the word from numberWords array
+    let classes = `Die fas fa-dice-${numberWords[val - 1]
+    } fa-5x `;
+    if (locked) classes += "Die-locked";
+    if (rolling) classes += 'Die-rolling';
+    return (
+      <i
+        className={classes}
+        onClick={() => this.props.handleClick(this.props.idx)} 
+        disabled={disabled}
+      />
+      
+    );
+  }
+}
+
+export default Die;
+
+
+//Not passing the index in onClick (ln 10)
+  //Need to pass in the index from the Die component
